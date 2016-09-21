f608474162bc9fc877427743678ee4dedc52607d
Sara <sarabertaina@hotmail.it>
2016-09-21 11:45:34 +0200

    Deploy v0.1.33

--- vendor/ckeditor/config.js
+++ vendor/ckeditor/config.js
@@ -7,4 +7,6 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+  config.enterMode = CKEDITOR.ENTER_BR;
+  config.shiftEnterMode = CKEDITOR.ENTER_P;
 };
